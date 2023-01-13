006a9959dfc32a10dc8c9cac4f148e86ee43a75e
Ann S <gb9363@mail.ru>
2023-01-13 05:19:02 +0300

    Ex010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {17, 12, 53, 4, 25, 4, 47, 2};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+index++;
+}
